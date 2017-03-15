f7b2b196fc7fed83f99e4710e89cd61127593c4a
Ankush Sharma <ankushs@smartdatainc.net>
2017-03-15 14:49:11 +0530

    first commit

---
+++ app/components/Home.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import Messages from './Messages';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Messages messages={this.props.messages}/>
+        <div className="row">
+          <div className="col-sm-4">
+            <div className="panel">
+              <div className="panel-body">
+                <h3>Heading</h3>
+                <p>This is a dummy text. This is a dummy text. This is a dummy text. This is a dummy text. This is a dummy text. This is a dummy text. This is a dummy text. This is a dummy text. This is a dummy text. </p>
+                <a href="#" role="button" className="btn btn-default">View details</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="panel">
+              <div className="panel-body">
+                <h3>Heading</h3>
+                <p>This is a dummy text. This is a dummy text. This is a dummy text. This is a dummy text. This is a dummy text. This is a dummy text. This is a dummy text. </p>
+                <a href="#" role="button" className="btn btn-default">View details</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="panel">
+              <div className="panel-body">
+                <h3>Heading</h3>
+                <p>This is a dummy text. This is a dummy text. This is a dummy text. This is a dummy text. </p>
+                <a href="#" role="button" className="btn btn-default">View details</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    messages: state.messages
+  };
+};
+
+export default connect(mapStateToProps)(Home);
